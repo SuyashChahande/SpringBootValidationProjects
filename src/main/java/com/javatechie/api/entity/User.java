6160ebfdb1a99ac03ed9c081ccf581e962037131
SuyashChahande <suyashchahande77@gmail.com>
2024-01-19 19:20:54 +0530

    SpringBootCrudProject

---
+++ src/main/java/com/javatechie/api/entity/User.java
@@ -0,0 +1,28 @@
+package com.javatechie.api.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Entity
+@Table(name = "USERS_TBL")
+@Data
+@AllArgsConstructor(staticName = "build")
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue
+    private int userId;
+    private String name;
+    private String email;
+    private String mobile;
+    private String gender;
+    private int age;
+    private String nationality;
+}
